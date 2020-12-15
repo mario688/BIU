7c2a338726183e8741aa21909d5a0f8eefcd3be0
mario688 <gliwamario@gmail.com>
2020-12-15 23:17:48 +0100

    React stepik zad4

---
+++ web-app/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react'
+
+
+export class Footer extends Component{
+    render(){
+        return(
+           
+              <footer>
+                <p>Copyright by Bogate Interfejsy Webowe</p>
+              </footer>
+              
+
+        )
+    }
+
+}
---
+++ web-app/src/components/Navigation.js
@@ -0,0 +1,21 @@
+import React,{Component} from 'react'
+
+
+export class Navigation extends Component{
+    render(){
+        return(
+            <div>
+                <p>Navigation bar</p>
+               <nav>
+                 <ul>
+                 <li>Home</li>
+                 <li>About</li>
+                 <li>Contact</li>
+              </ul>
+             </nav>
+            </div>    
+
+        )
+    }
+
+}
